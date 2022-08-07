01b25709ce6866e37f5dab45d15f0d0cf98edc0c
Mahir Molai <mahir@molai.dev>
2022-08-08 00:25:59 +0500

    add credits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,14 @@
 </script>
 
 <div class="container">
-	<h1>gScript©️</h1>
-	<div class="links">
-		<button on:click={() => redirect("https://www.npmjs.com/package/gscript-compiler")}>Compiler</button>
-		<button on:click={() => redirect("https://github.com/gscript-lang/gscript")}>GitHub</button>
+	<div class="top">
+		<h1>gScript©️</h1>
+		<div class="links">
+			<button on:click={() => redirect("https://www.npmjs.com/package/gscript-compiler")}>Compiler</button>
+			<button on:click={() => redirect("https://github.com/gscript-lang/gscript")}>GitHub</button>
+		</div>
 	</div>
+	<h3>Made with ❤ by <a class="fx" href="https://molai.dev/">molai.dev</a></h3>
 </div>
 
 <style lang="scss">
@@ -20,19 +23,26 @@
 		width: 100vw;
 		display: flex;
 		align-items: center;
-		justify-content: center;
-		flex-direction: center;
+		justify-content: space-between;
 		flex-direction: column;
 		gap: 25px;
 		font-family: 'Josefin Sans', sans-serif;
 
-		h1 {
-			color: white;
-			font-weight: 700;
-			font-size: 86px;
-		}
+		.top {
+			display: flex;
+			margin-top: 350px;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			gap: inherit;
 
-		.links {
+			h1 {
+				color: white;
+				font-weight: 700;
+				font-size: 86px;
+			}
+
+			.links {
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
@@ -40,7 +50,7 @@
 			flex-direction: row;
 			gap: 25px;
 			font-family: 'Josefin Sans', sans-serif; 
-
+			
 			button {
 				background: #000;
 				color: white;
@@ -52,12 +62,47 @@
 				font-size: 24px;
 				transition: 600ms ease-in-out;
 				cursor: pointer;
-
+			
 				&:hover {
 					background: #fff;
 					color: black;
 				}
 			}
+			}
 		}
+
+		h3 {
+			color: white;
+			font-size: 24px;
+			margin-bottom: 25px;
+
+			a {
+				color: rgb(130, 130, 130);
+				text-decoration: none;
+			}
+		}
+
+		.fx {
+        	    border: none;
+        	    display: inline;
+        	    position: relative;
+        	    cursor: pointer;
+        	    &:after {
+        	        content: '';
+        	        position: absolute;
+        	        width: 100%;
+        	        transform: scaleX(0);
+        	        height: 2px;
+        	        bottom: 0;
+        	        left: 0;
+        	        background-color: #0087ca;
+        	        transform-origin: bottom right;
+        	        transition: transform 0.25s ease-out;
+        	    }	
+        	    &:hover:after{
+        	        transform: scaleX(1);
+        	        transform-origin: bottom left;
+        	    }
+        	}
 	}
 </style>
